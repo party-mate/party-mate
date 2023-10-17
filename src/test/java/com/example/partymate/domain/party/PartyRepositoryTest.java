030c5de3e708d0cbe7ecb6d40eed3f3e54a6d944
Unagi_zoso <Unagi.zoso@gmail.com>
2023-10-17 14:48:52 +0900

    Test: PartyRepository 테스트 클래스 추가

---
+++ src/test/java/com/example/partymate/domain/party/PartyRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.partymate.domain.party;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-10-13
+ */
+class PartyRepositoryTest {
+
+}
