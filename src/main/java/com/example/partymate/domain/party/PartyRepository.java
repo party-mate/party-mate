3a62272316e77be38cff2cba1573ff734cab9edc
Unagi_zoso <Unagi.zoso@gmail.com>
2023-10-17 14:42:10 +0900

    Feat: Party Repository 추가

---
+++ src/main/java/com/example/partymate/domain/party/PartyRepository.java
@@ -0,0 +1,10 @@
+package com.example.partymate.domain.party;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-10-13
+ */
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
