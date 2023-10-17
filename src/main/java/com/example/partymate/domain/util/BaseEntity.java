2ff99bd5a7eeedec397d962f852258cb08cee196
Unagi_zoso <Unagi.zoso@gmail.com>
2023-10-17 14:44:39 +0900

    Feat: 생성날짜, 수정날짜를 자동으로 저장하는 속성을 가진 상위 엔티티 클래스 생성

---
+++ src/main/java/com/example/partymate/domain/util/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.partymate.domain.util;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-10-13
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    LocalDateTime updatedDateTime;
+}
