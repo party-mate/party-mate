ff23c86474f2976b15cfb1f9bb700b27af6f0cb1
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-03 15:56:32 +0900

    Feat: Role 엔티티 추가

---
+++ src/main/java/com/example/partymate/domain/Role/Role.java
@@ -0,0 +1,40 @@
+package com.example.partymate.domain.Role;
+
+import com.example.partymate.domain.member.Member;
+import com.example.partymate.domain.util.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-10-17
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Role extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column(nullable = false)
+    private String roleName;
+
+    @OneToOne
+    @JoinColumn(name = "role")
+    private Member member;
+}
