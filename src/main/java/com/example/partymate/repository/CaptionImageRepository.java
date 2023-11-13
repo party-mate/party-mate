b8d84c7681150ec24535c853c6d732a74cec3a7f
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-13 23:29:49 +0900

    Feat: CaptionImageHelper Jpa Repository

---
+++ src/main/java/com/example/partymate/repository/CaptionImageRepository.java
@@ -0,0 +1,11 @@
+package com.example.partymate.repository;
+
+import com.example.partymate.model.CaptionImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-12
+ */
+public interface CaptionImageRepository extends JpaRepository<CaptionImage, Long> {
+}
