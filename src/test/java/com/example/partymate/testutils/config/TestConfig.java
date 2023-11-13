baeb96da7eb431d33ace9a91d1f05d0bce5dcebf
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-13 23:07:11 +0900

    Feat: 테스트시 설정

---
+++ src/test/java/com/example/partymate/testutils/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.example.partymate.testutils.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-08
+ */
+@TestConfiguration
+public class TestConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
