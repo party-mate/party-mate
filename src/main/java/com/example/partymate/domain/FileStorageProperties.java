4a7c5d0821305195130c8c19780f76dce1117de2
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-14 00:25:39 +0900

    Feat: File 저장 위치 관리 클래스

---
+++ src/main/java/com/example/partymate/domain/FileStorageProperties.java
@@ -0,0 +1,19 @@
+package com.example.partymate.domain;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Unagi_zoso
+ * @since 2023-11-12
+ */
+@Getter
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+
+    private String uploadDir;
+
+    public void setUploadDir(String uploadDir) {
+            this.uploadDir = uploadDir;
+        }
+}
