dc4c29380ff3240d8efc4ef6edcbe468debd6026
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-13 23:58:29 +0900

    Feat: MemberService 추가

---
+++ src/main/java/com/example/partymate/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.partymate.service;
+
+import static com.example.partymate.model.Member.toMember;
+import com.example.partymate.dto.MemberSaveRequestDto;
+import com.example.partymate.domain.FileStorageProperties;
+import com.example.partymate.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : Unagi_zoso
+ * @since : 2023-11-12
+ */
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+    public final MemberRepository memberRepository;
+
+    public void saveMember(MemberSaveRequestDto memberSaveRequestDto) {
+        FileStorageService fileStorageService = new FileStorageService(new FileStorageProperties());
+        String profileImageUrl = fileStorageService.storeFile(memberSaveRequestDto.getProfileImage());
+        validateAgreement(memberSaveRequestDto);
+        memberRepository.save(toMember(memberSaveRequestDto, profileImageUrl));
+    }
+
+    private void validateAgreement(MemberSaveRequestDto memberSaveRequestDto) {
+        if (memberSaveRequestDto.getAgreement().getAgreePrivacyFlag() == 0 ||
+                memberSaveRequestDto.getAgreement().getAgreeServiceFlag() == 0 ||
+                memberSaveRequestDto.getAgreement().getAgreeMarketingFlag() == 0) {
+            throw new IllegalArgumentException("약관에 동의해주세요.");
+        }
+    }
+
+    public void updateMember(Long memberId, String name, String email, String password) {
+        // memberRepository.save (memberId, name, email, password);
+    }
+
+    public void deleteMember(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
+}
