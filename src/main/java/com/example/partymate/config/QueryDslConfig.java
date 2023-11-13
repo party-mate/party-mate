38329c83bb5a7dd2a9ee3a7eb0e92d26edf7f232
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-13 23:08:15 +0900

    Feat: QueryDSL config 클래스

---
+++ src/main/java/com/example/partymate/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package com.example.partymate.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Unagi_zoso
+ * @date : 2023-11-08
+ */
+@Configuration
+public class QueryDslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
