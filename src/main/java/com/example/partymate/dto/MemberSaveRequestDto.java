e67ff06d74811894071bdc8b402ea235c2bdae7f
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-14 15:07:36 +0900

    Feat: dto용 Lombok 어노테이션 추가

--- src/main/java/com/example/partymate/dto/MemberSaveRequestDto.java
+++ src/main/java/com/example/partymate/dto/MemberSaveRequestDto.java
@@ -1,24 +1,24 @@
 package com.example.partymate.dto;
 
 import com.example.partymate.model.Agreement;
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
+import lombok.NoArgsConstructor;
 
 /**
  * @author : Unagi_zoso
  * @since : 2023-11-12
  */
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MemberSaveRequestDto {
     private String password;
     private String emailAddress;
-    private MultipartFile profileImage;
     private String phoneNumber;
     private String nickname;
     private String gender;
-    private LocalDate birthYearDate;
     private Agreement agreement;
 }
