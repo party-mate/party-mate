7fd147536adde802ca4349f9fe4365a1070d759f
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-15 19:20:01 +0900

    Feat: Party 서비스 구현

--- src/main/java/com/example/partymate/service/PartyService.java
+++ src/main/java/com/example/partymate/service/PartyService.java
@@ -1,8 +1,24 @@
 package com.example.partymate.service;
 
+import static com.example.partymate.model.Party.toParty;
+
+import com.example.partymate.dto.PartySaveRequestDto;
+import com.example.partymate.repository.PartyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Unagi_zoso
  * @since 2023-11-13
  */
+@Service
+@RequiredArgsConstructor
 public class PartyService {
+    private final PartyRepository partyRepository;
+    private final MemberService memberService;
+    private final PostService postService;
+
+    public void saveParty(PartySaveRequestDto partySaveRequestDto) {
+        partyRepository.save(toParty(partySaveRequestDto));
+    }
 }
