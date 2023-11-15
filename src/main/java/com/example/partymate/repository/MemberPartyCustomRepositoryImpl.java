4ee1b53b47b0cefab0b89b627c41556c3a40d1de
Unagi_zoso <Unagi.zoso@gmail.com>
2023-11-15 23:08:01 +0900

    Chore: Import 정리

--- src/main/java/com/example/partymate/repository/MemberPartyCustomRepositoryImpl.java
+++ src/main/java/com/example/partymate/repository/MemberPartyCustomRepositoryImpl.java
@@ -1,24 +1,19 @@
 package com.example.partymate.repository;
 
-import com.example.partymate.dto.MemberPartyResponseDto;
-import com.example.partymate.dto.MemberPartyResponseDtoList;
-import com.example.partymate.dto.PostResponseDto;
-import com.example.partymate.dto.PostResponseDtoList;
-import com.example.partymate.dto.QMemberPartyResponseDto;
-import static com.example.partymate.model.QCaptionImage.captionImage;
-import static com.example.partymate.model.QCategory.category;
 import static com.example.partymate.model.QMember.member;
 import static com.example.partymate.model.QMemberParty.memberParty;
 import static com.example.partymate.model.QParty.party;
-import static com.example.partymate.model.QPost.post;
-import com.querydsl.core.types.Projections;
+
+import com.example.partymate.dto.MemberPartyResponseDto;
+import com.example.partymate.dto.MemberPartyResponseDtoList;
+import com.example.partymate.dto.QMemberPartyResponseDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 
 /**
- * @author : Unagi_zoso
- * @date : 2023-11-12
+ * @author Unagi_zoso
+ * @since 2023-11-12
  */
 @Repository
 public class MemberPartyCustomRepositoryImpl implements MemberPartyCustomRepository{
