8d156bab3b168bc993864f80d4cbd3924c3ce231
JJDabean <wjdekqls406@gmail.com>
2023-11-17 19:07:30 +0900

    Feat: 로그인후 페이지 이동경로

--- src/main/java/com/example/partymate/config/SecurityConfig.java
+++ src/main/java/com/example/partymate/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.example.partymate.config;
 
+import com.example.partymate.config.auth.AuthenticationProviderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +26,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         setLocalMode(http);
+        http.formLogin().defaultSuccessUrl("/",true);
+        http.authorizeRequests().anyRequest().authenticated();
         return http.build();
     }
 
